5e5a1388b8ee03352dcbe6c9bcf81b7bb13b3b36
queSaDiLLaSS <118369217+queSaDiLLaSS@users.noreply.github.com>
2022-12-06 17:01:45 +0100

    Update Propriete.cpp

--- source/Propriete.cpp
+++ source/Propriete.cpp
@@ -1,4 +1,13 @@
 #include "Propriete.h"
+#include <string>
+using namespace std;
+
+
+Propriete::Propriete(string nom, int loyer, int prixAchat) : Case(nom)
+{
+    this->loyer = loyer;
+    this->prixAchat = prixAchat;
+}
 
 int Propriete::getLoyer()
 {
