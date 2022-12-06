e7733cffaaa58e55325c64ec78985f2dde2d9791
henritaing <95174330+henritaing@users.noreply.github.com>
2022-12-06 15:38:00 +0100

    Create plateau.h

---
+++ header/plateau.h
@@ -0,0 +1,23 @@
+#pragma once
+#include <string>
+using namespace std;
+#include "Case.h"
+
+#ifndef Plateau_H
+#define Plateau_H
+
+#include <list>
+
+class Plateau
+{
+	private:
+		case Case;
+
+	public:
+    //Constructeur
+    case createCases();
+   
+    //lien entre les cases
+    void lierCases();
+    
+};
