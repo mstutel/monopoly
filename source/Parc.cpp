0d3e3059dc209134dcd79ac9508cfa67a6be8df4
queSaDiLLaSS <118369217+queSaDiLLaSS@users.noreply.github.com>
2022-12-18 15:44:17 +0100

    Update Parc.cpp

--- source/Parc.cpp
+++ source/Parc.cpp
@@ -1,9 +1,9 @@
-#include "Gare.h"
+#include "Parc.h"
 
-Gare::Gare(string nom, int prixAchat, int loyer) : Propriete(nom, prixAchat, loyer)
+Parc::Parc(string nom) : Case(nom)
 {
 }
 
-void Gare::arreterSur()
+void Parc::arreterSur()
 {
 }
