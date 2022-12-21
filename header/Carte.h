66073170510cbc1123d5aae5e72aa57db7585cfe
jtengo <104306874+jtengo@users.noreply.github.com>
2022-12-21 15:29:03 +0100

    Update Carte.h
    ajout des variables isLiberation et isReparation dans la définition de la classe Carte

--- header/Carte.h
+++ header/Carte.h
@@ -11,12 +11,12 @@ private :
   bool positionGlobale; /* vaut true si le changement de position est "global" (par exemple "Allez à la Rue de la Paix") et false sinon (par exemple "Réculez de 3 cases") */
   int changePostion;
   string description;
+  bool isLiberation;
+  bool isReparation;
   
  
 public :
-  Carte(int, int,bool, int,string);
+  Carte(int, int,bool, int,string,int isLiberation=false,int isReparation=false);
   void action(Joueur);
   
-
-
 };
