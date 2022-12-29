ed62fc699660aa24591a01419ab596531c2ec9ea
jtengo <104306874+jtengo@users.noreply.github.com>
2022-12-29 13:43:30 +0100

    Update ListeCartes.cpp

--- source/ListeCartes.cpp
+++ source/ListeCartes.cpp
@@ -8,7 +8,7 @@ ListeCartes::ListeCartes() {
    listeChance[1]=Carte(-20, false, 0,"Amende pour ivresse : 20€.",&listeChance[0]);
    listeChance[2]=Carte(-15, false, 0,"Amende pour excès de vitesse : 15€.",&listeChance[1]);
    listeChance[3]=Carte(100, false, 0,"Vous avez gagné le prix de mots croisés. Recevez 100€.",&listeChance[2]);
-   listeChance[4]=Carte(-150, false, 0,"Payez pour frais de scolarité 150€.",listeChance[3]);
+   listeChance[4]=Carte(-150, false, 0,"Payez pour frais de scolarité 150€.",&listeChance[3]);
    listeChance[5]=Carte(150, false, 0,"Votre immeuble et votre prêt rapportent. Vous devez toucher 150€.",&listeChance[4]);
    listeChance[6]=Carte(0,true,24,"Rendez-vous à l'Avenue Henri-Martin.",&listeChance[5]);
    listeChance[7]=Carte(0,true,30,"Allez en prison.",&listeChance[6]);
@@ -16,6 +16,7 @@ ListeCartes::ListeCartes() {
    listeChance[9]=Carte(0,true,39,"Rendez-vous à la rue de la Paix.",&listeChance[8]);
    listeChance[10]=Carte(0,true,15,"Allez à la gare de Lyon."),&listeChance[9];
    listeChance[11]=Carte(0,true,11,"Avancez au Boulevard de la Vilette.",&listeChance[10]);
+   /*A modifier  :on ne peut pas différencier listeChance[12] et  listeChance[13] avec cette définition*/
    listeChance[12]=Carte(0,false,0,"Faites des réparations dans vos maisons. Versez pour chaque maison 25€ et versez pour chaque hôtel 100€.",&listeChance[11],false,true);
    listeChance[13]=Carte(0,false,0,"Faites des réparations dans vos maisons. Versez pour chaque maison 40€ et versez pour chaque hôtel 115€.",&listeChance[12],false,true);
    listeChance[14]=Carte(0,false,-3,"Reculez de trois cases.",&listeChance[13]);
@@ -42,8 +43,28 @@ ListeCartes::ListeCartes() {
    listeCommunaute[0].setSuivant(&listeCommunaute[13]);
    
    /* Initialisation des pointeurs de haut de pile pour leurs piles respectives*/
-   hautChance=&listeChance[0];
-   hautCommunaute=&listeCommunaute[0];
+   hautChance=&listeChance[15];
+   hautCommunaute=&listeCommunaute[13];
    
 
+}
+
+Carte ListeCartes:: tirageChance() {
+
+   Carte* precedent = hautChance;
+   cout<<"CARTE CHANCE : "<<endl
+      <<precedent->getDescription();
+   hautChance = hautChance->getSuivant();
+   return precedent;
+   
+}
+
+Carte ListeCartes:: tirageCommunaute() {
+
+   Carte* precedent = hautCommunaute;
+   cout<<"CARTE COMMUNAUTE : "<<endl
+      <<precedent->getDescription();
+   hautCommunaute = hautCommunaute->getSuivant();
+   return precedent;
+   
 }
