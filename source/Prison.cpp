79f05a28ff028f346c9f7a806cdd63268280bc48
Adrian <118369217+queSaDiLLaSS@users.noreply.github.com>
2023-01-03 12:03:20 +0100

    Update Prison.cpp

--- source/Prison.cpp
+++ source/Prison.cpp
@@ -1,8 +1,13 @@
 #include "Prison.h"
+#include "Jeu.h"
 
 Prison :: Prison(string nom) : Case(nom) {
 }
 
-void Prison::arreterSur()
+void Prison::arreterSur(Joueur joueur)
 {
+	if (nom == "Aller en Prison") {
+		joueur.avancer(20);
+	}
+  //FAIRE LE CAS SI ON EST EN PRISON 
 }
