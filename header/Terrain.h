3d0ef0565a5bb8461b639138f900691b5d3d59f5
Adrian <118369217+queSaDiLLaSS@users.noreply.github.com>
2023-01-10 13:17:59 +0100

    Update Terrain.h

--- header/Terrain.h
+++ header/Terrain.h
@@ -1,17 +1,30 @@
 #pragma once
 #include "Propriete.h"
+using namespace std;
 
 class Terrain : public Propriete
 {
-	private:
-		int maison1;
-		int maison2;
-		int maison3;
-		int maison4;
-		int hotel;
-		int prixMaison;
-		
-	public:
-		Terrain(string nom, int prixAchat, int loyer, int maison1, int maison2, int maison3, int maison4, int hotel, int prixMaison);
-		void arreterSur();
+  private:
+    int loyermaison1;
+    int loyermaison2;
+    int loyermaison3;
+    int loyermaison4;
+    int loyerhotel;
+    int prixMaison;
+    string couleur;
+
+
+  public:
+    //Constructeur, nom de la propriété, son prix d'achat, son loyer, nb de maisons, prix maison, propriétaire, couleur
+    Terrain(string nom, int prixAchat, int loyer, int loyermaison1, int loyermaison2, int loyermaison3, int loyermaison4, int loyerhotel, int prixMaison, string couleur);
+    
+    void listeloyer();
+    
+    //Checker si le propriétaire possède toutes les propriétés de la même couleur
+    bool checkCouleur(Joueur joueur);
+    
+    //Prix de la maison en fonction de la case
+    void acheterMaison(Joueur joueur);
+
+    void arreterSur(Joueur joueur);
 };
