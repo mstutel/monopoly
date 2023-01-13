3dbe0d0c5002a71f6a000c3a9907c97f99db19d3
Adrian <118369217+queSaDiLLaSS@users.noreply.github.com>
2023-01-13 12:02:29 +0000

    groupe

---
+++ source/Groupe.cpp
@@ -0,0 +1,27 @@
+#include "Groupe.h"
+
+Groupe::Groupe(Case a, Case b)
+{
+    listeElements[0] = &a;
+    listeElements[1] = &b;
+    listeElements[2] = nullptr;
+    taille = 2;
+}
+
+Groupe::Groupe(Case a, Case b, Case c)
+{
+    listeElements[0] = &a;
+    listeElements[1] = &b;
+    listeElements[2] = &c;
+    taille = 3;
+}
+
+int Groupe::getTaille()
+{
+    return taille;
+}
+
+Case *Groupe::getElement(int index)
+{
+    return listeElements[index];
+}
